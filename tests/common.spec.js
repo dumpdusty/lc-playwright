88f2598666c265841dc4faf2ee3fad0db8f1a508
Dusty Dump <dumpdusty@gmail.com>
2023-09-09 16:58:47 -0700

    install and config prettier

--- tests/common.spec.js
+++ tests/common.spec.js
@@ -1,35 +1,37 @@
-import { test, expect } from '@playwright/test';
-import {logIn} from "../common/login";
+import { test, expect } from "@playwright/test";
+import { logIn } from "../common/login";
 
-test.describe('Navigation', () => {
-    test.beforeEach(async({page}) => {
-        await logIn(page, process.env.EMAIL, process.env.PASSWORD)
-    })
-    test.afterEach(async ({ page }, testInfo) => {
-        console.log(`Finished ${testInfo.title} with status ${testInfo.status}`)
-    });
+test.describe("Navigation", () => {
+  test.beforeEach(async ({ page }) => {
+    await logIn(page, process.env.EMAIL, process.env.PASSWORD);
+  });
+  test.afterEach(async ({ page }, testInfo) => {
+    console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
+  });
 
-    test('verify Courses link', async ({ page }) => {
-        await page.getByTestId('topmenu-Курсы').click()
-        await expect(page).toHaveURL('/course')
-        await expect(page.getByText('Курсы программирования и тестирования')).toBeVisible()
-    });
+  test("verify Courses link", async ({ page }) => {
+    await page.getByTestId("topmenu-Курсы").click();
+    await expect(page).toHaveURL("/course");
+    await expect(
+      page.getByText("Курсы программирования и тестирования"),
+    ).toBeVisible();
+  });
 
-    test('verify Tasks link', async ({ page }) => {
-        await page.getByTestId('topmenu-Задачи').click()
-        await expect(page).toHaveURL('/challenge?limit=30&page=1')
-        await expect(page.getByText('Кодинг задачи')).toBeVisible()
-    })
+  test("verify Tasks link", async ({ page }) => {
+    await page.getByTestId("topmenu-Задачи").click();
+    await expect(page).toHaveURL("/challenge?limit=30&page=1");
+    await expect(page.getByText("Кодинг задачи")).toBeVisible();
+  });
 
-    test('verify Interview link', async ({ page }) => {
-        await page.getByTestId('topmenu-Интервью').click()
-        await expect(page).toHaveURL('/flash')
-        await expect(page.getByText('Interview practice cards')).toBeVisible()
-    })
+  test("verify Interview link", async ({ page }) => {
+    await page.getByTestId("topmenu-Интервью").click();
+    await expect(page).toHaveURL("/flash");
+    await expect(page.getByText("Interview practice cards")).toBeVisible();
+  });
 
-    test('verify Diary link', async ({ page }) => {
-        await page.getByTestId('topmenu-Дневник').click()
-        await expect(page).toHaveURL('/diary?page=1')
-        await expect(page.getByText('Daily reports')).toBeVisible()
-    })
-})
+  test("verify Diary link", async ({ page }) => {
+    await page.getByTestId("topmenu-Дневник").click();
+    await expect(page).toHaveURL("/diary?page=1");
+    await expect(page.getByText("Daily reports")).toBeVisible();
+  });
+});
